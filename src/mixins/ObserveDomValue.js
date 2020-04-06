709862f2871088c3e95d944cf55419c8740fab0e
David Callizaya <davidcallizaya@gmail.com>
2020-04-06 11:50:45 -0400

    Fix dom binding

--- src/mixins/ObserveDomValue.js
+++ src/mixins/ObserveDomValue.js
@@ -10,26 +10,42 @@ export default {
       this.node = this.value;
       this.$forceUpdate();
     },
+    bindToDomValue() {
+      if (!this.value) {
+        return;
+      }
+      this.mutationObserver = new MutationObserver((mutations) => {
+        const attributes = mutations.find(mutation => mutation.type === 'attributes');
+        const childList = mutations.find(mutation => mutation.type === 'childList');
+        const subtree = mutations.find(mutation => mutation.type === 'subtree');
+        if (attributes || childList || subtree) this.valueChanged();
+      });
+      this.mutationObserver.disconnect();
+      this.mutationObserver.observe(this.value, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+      });
+    },
+    unbindToDomValue() {
+      if (this.mutationObserver) {
+        this.mutationObserver.disconnect();
+      }
+    },
   },
   mounted() {
     this.node = this.value;
-    if (!this.value) {
-      return;
-    }
-    this.mutationObserver = new MutationObserver((mutations) => {
-      const attributes = mutations.find(mutation => mutation.type === 'attributes');
-      const childList = mutations.find(mutation => mutation.type === 'childList');
-      const subtree = mutations.find(mutation => mutation.type === 'subtree');
-      if (attributes || childList || subtree) this.valueChanged();
-    });
-    this.mutationObserver.disconnect();
-    this.mutationObserver.observe(this.value, {
-      attributes: true,
-      childList: true,
-      subtree: true,
-    });
+    this.bindToDomValue();
   },
   destroyed() {
-    this.mutationObserver.disconnect();
-  }
+    this.unbindToDomValue();
+  },
+  watch: {
+    value() {
+      this.node = null;
+      this.node = this.value;
+      this.unbindToDomValue();
+      this.bindToDomValue();
+    }
+  },
 }
